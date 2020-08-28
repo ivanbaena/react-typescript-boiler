cd5e5a5c1e83fd86b137f9d224ac383898df50ba
Ivan Baena <baenadev.io@gmail.com>
2020-08-28 02:13:31 -0400

    fetch and delete action creators

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Todo, fetchTodos, deleteTodo } from '../actions/';
+import { StoreState } from '../reducers';
+import { DeleteTodoAction, FetchTodosAction } from '../actions';
+
+interface AppProps {
+  todos: Todo[];
+  fetchTodos: typeof fetchTodos;
+  deleteTodo: typeof deleteTodo;
+}
+
+interface AppState {
+  fetching: boolean;
+}
 
 export const App = () => {
-  return <div>Hi There!</div>;
+  const [fetching, setFetching] = useState(false);
+  const todos = useSelector((state: StoreState) => state.todos);
+  const dispatch = useDispatch();
+
+  const onBtnClick = (): any => {
+    setFetching(true);
+    dispatch(fetchTodos());
+  };
+
+  const deleteOnClick = (id: number): DeleteTodoAction =>
+    dispatch(deleteTodo(id));
+
+  const renderList = (data: Todo[]): JSX.Element => {
+    const list = todos.map((todo) => {
+      return (
+        <li key={todo.id}>
+          {todo.title}
+          <button onClick={() => deleteOnClick(todo.id)}>Delete</button>
+        </li>
+      );
+    });
+    return <ul>{list}</ul>;
+  };
+
+  useEffect(() => {
+    return () => {
+      setFetching(false);
+    };
+  }, [todos]);
+
+  return (
+    <div>
+      <button onClick={onBtnClick}>Fetch</button>
+      {fetching ? 'loading' : null}
+      {renderList(todos)}
+    </div>
+  );
 };
